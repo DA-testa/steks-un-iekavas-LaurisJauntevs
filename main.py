0fd6e670a81a6d14cbc121cea5ece2bd9f505c54
LaurisJauntevs <124596308+LaurisJauntevs@users.noreply.github.com>
2023-02-19 23:47:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i+1))
-            pass
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-            if not opening_brackets_stack:
-                return "Sucess"
-            else:
-                return opening_brackets_stack[-1].position
+    if not opening_brackets_stack:
+        return "Sucess"
+    else:
+        return opening_brackets_stack[-1].position
            
             # Process closing bracket, write your code here
 
