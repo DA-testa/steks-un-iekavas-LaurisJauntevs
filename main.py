fe6039c19b7a7a6c2d01c308309bcd272011e06b
LaurisJauntevs <124596308+LaurisJauntevs@users.noreply.github.com>
2023-02-19 23:11:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
-def find_mismatch(text):
+def find_mismatch(text): 
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
@@ -18,24 +18,36 @@ def find_mismatch(text):
             pass
 
         if next in ")]}":
-            opening_brackets_stack.append(Bracket(next,i+2))
-            if are_matching==(opening_brackets_stack(next, i+1),opening_brackets_stack[-1]):
-                opening_brackets_stack.pop()
-            else:
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
+            if opening_brackets_stack.pop():
+                return opening_brackets_stack[-1].position
+            return "Sucess"
             # Process closing bracket, write your code here
             pass
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if text ==
-        print("Sucess")
+    x = input("input I or F:")
+    if "F" in x:
+        path = input("Input file path:")
+        with open(path, "r") as file:
+            a = file.read()
+            mismatch = find_mismatch(a)
+            if mismatch == 0:
+                print("Sucess")
+            else:
+                print(mismatch)
+    if "I" in x:
+        a = input()
+        mismatch = find_mismatch(a)
+        if mismatch == 0:
+                print("Sucess")
+        else:
+                print(mismatch)
     else:
-        print(mismatch)
+         print("Wrong input")
+    # Printing answer, write your code here
 
 if __name__ == "__main__":
     main()
-
